7276654bb793a47701a0f2249dbd3becddfbad9d
Astrit <astrit989@gmail.com>
2020-06-02 20:10:44 -0500

    02.06.20 commit

---
+++ src/test/java/com/weborders/tests/SmokeTest.java
@@ -0,0 +1,34 @@
+package com.weborders.tests;
+
+import com.weborders.pages.LoginPage;
+
+import static org.testng.Assert.*;
+
+import com.weborders.utilities.BrowserUtilities;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class SmokeTest extends AbstractBaseTest {
+
+    @Test(dataProvider = "smokeTestData")
+    public void smokeTest(String component, String expectedPageSubTitle) {
+        extentTest = extentReports.createTest("Verify " + component);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        loginPage.navigateTo(component);
+        BrowserUtilities.wait(2);
+        assertEquals(loginPage.getPageSubtitleText(), expectedPageSubTitle);
+
+        extentTest.pass(component + " verified!");
+    }
+
+    @DataProvider(parallel = false) // to execute all tests in parallel
+    public Object[][] smokeTestData() {
+        return new Object[][]{
+                {"View all orders", "List of All Orders"},
+                {"View all products", "List of Products"},
+                {"Order", "Order"}
+        };
+    }
+}
